1c30a33268bcc6a1d76816af95f02a2aeba8cd3f
837477 <8374770@gmail.com>
2021-01-31 04:00:49 +0900

    Create 모의고사.py

---
+++ src/LEVEL1/모의고사.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    h1 = [1, 2, 3, 4, 5]
+    h2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    h3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    cnt = [0] * 3
+    
+    for idx, value in enumerate(answers):
+        if value == h1[idx%5]:
+            cnt[0] += 1
+        if value == h2[idx%8]:
+            cnt[1] += 1
+        if value == h3[idx%10]:
+            cnt[2] += 1
+
+    answer = []
+    max_ = max(cnt)
+    for i in range(3):
+        if cnt[i] == max_:
+            answer.append(i+1)
+    
+    return answer
