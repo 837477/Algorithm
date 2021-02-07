bdbb1ae44bb830a6e5559a9925737cc594deded8
837477 <8374770@gmail.com>
2021-02-08 01:53:52 +0900

    Create N개의 최소공배수.py

---
+++ src/LEVEL2/N개의 최소공배수.py
@@ -0,0 +1,19 @@
+def solution(arr):
+    max_ = min(arr)
+    arr.remove(max_)
+
+    num = max_
+    while True:
+        flag = True
+        for a in arr:
+            if num % a != 0:
+                flag = False
+                break
+            
+        if flag:
+            return num
+
+        num += max_
+
+if __name__ == "__main__":
+    print(solution([2, 6, 8, 14]))
