52e56b245116c9a87bcb8043c1cd2b2026821ed1
837477 <8374770@gmail.com>
2021-08-31 22:48:59 +0900

    Create 땅따먹기.py

---
+++ src/LEVEL2/땅따먹기.py
@@ -0,0 +1,9 @@
+def solution(land):
+    for i in range(1,len(land)):
+        for j in range(len(land[0])):
+            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+    return max(land[len(land)-1])
+
+
+if __name__ == "__main__":
+    print(solution([[4, 3, 2, 1], [2, 2, 2, 1], [6, 6, 6, 4], [8, 7, 6, 5]]))
